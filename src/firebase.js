70e07cc945a3f21928e96763f2103ab85e7e88fb
pooja-salucro <prayphale@salucro.com>
2021-12-28 00:15:10 +0530

    Updated files and removed unnecessery files.

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAXx7ugQ_Vicnw1iJ1UP3D-jTHRqIILxpI",
+  authDomain: "react-slack-e61cb.firebaseapp.com",
+  projectId: "react-slack-e61cb",
+  storageBucket: "react-slack-e61cb.appspot.com",
+  messagingSenderId: "807125997644",
+  appId: "1:807125997644:web:5e8e24f6889209e7ed3ce9"
+};
