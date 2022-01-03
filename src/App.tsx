259c33769bd46cede36530fab2754f5ae63d2a4b
pooja-salucro <prayphale@salucro.com>
2022-01-03 23:37:24 +0530

    Added spiner, Added packged

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,26 @@ import Header from "./components/Headers";
 import Login from "./components/Login";
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import Spinner from "react-spinkit";
 import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 function App() {
   const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <div className="AppLoading">
+        <div className="AppLoadingContents">
+          <img
+            src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+            alt=""
+          />
+          <Spinner name="ball-spin-fade-loader" color="purple" fadeIn="none" />
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
--- src/components/Sidebar/SidebarHeader/SidebarHeader.tsx
+++ src/components/Sidebar/SidebarHeader/SidebarHeader.tsx
@@ -1,14 +1,18 @@
 import "./SidebarHeader.scss";
 
 import React from "react";
+import { auth } from "src/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function SidebarHeader() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="SidebarHeader__container">
       <div className="SidebarHeader__info">
         <h2>Fun Discussion</h2>
         <h3>
-          <i className="fas fa-circle" /> John
+          <i className="fas fa-circle" /> {user?.displayName}
         </h3>
       </div>
       <div className="SidebarHeader__pencilIcon">
